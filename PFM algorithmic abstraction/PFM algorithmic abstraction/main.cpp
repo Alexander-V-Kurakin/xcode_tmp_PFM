c991aab87b7e0f346c88d0f2eec94fd83d3f7c77
MUC_Mercury <alexander.kurakin@t-online.de>
2023-11-24 15:02:01 +0100

    A vertex signatures assignment at compile time

--- PFM algorithmic abstraction/PFM algorithmic abstraction/main.cpp
+++ PFM algorithmic abstraction/PFM algorithmic abstraction/main.cpp
@@ -7,47 +7,99 @@
 
 #include <iostream>
 
-int G = 0;          // GSR(G) as global signature register
+using namespace std;
+
+#define TRACE(x) (cout << #x << " = " << x << "\t")
+
+int G = 0;                  // GSR(G) as global signature register
 
 // The list of vertices unique signatures
-#define V_0 0       // Initial condition, the vertex doesn't exist
-#define V_1 0
-#define V_2 0
-#define V_3 0
+#define V_0 0               // Initial condition, the vertex doesn't exist
+#define V_1 0x00000011
+#define V_2 0x00002200
+#define V_3 0x00440000
 
-int V[ 2 ][ 4 ] =   // Number of vertices having a unique signature
+const int V[ 2 ][ 4 ] =     // Number of vertices having a unique signature
 {
-    { 0, 1, 2, 3 },
-    {               // The list of D(d) = S(s) ^ S(d)
-        0,
-        0,
-        0,
-        0
+    { V_0, V_1, V_2, V_3 },
+    {                       // The list of values of D(d) = S(s) ^ S(d)
+                            // assigned at compile time, GSR(G) += D(d)
+        G ^ V_0,
+        G ^ V_0 ^ V_1,
+        G ^ V_0 ^ V_1 ^ V_2,
+        G ^ V_0 ^ V_1 ^ V_2 ^ V_3
     }
 };
 
-void F1( void )
+int F1( void )
 {
-//    int d = V[ 0 ][ 0 ] ^ V[ 0 ][ 1 ];
+    int d = V[ 1 ][ 0 ] ^ V[ 1 ][ 1 ];
+    
+    if (( G += d ) != V[ 1 ][ 1 ]) cout << "(illegal sequence)\t";
+    
+    return G;
 }
 
-void F2( void )
+int F2( void )
 {
+    int d = V[ 1 ][ 1 ] ^ V[ 1 ][ 2 ];
     
+    if (( G += d ) != V[ 1 ][ 2 ]) cout << "(illegal sequence)\t";
+    
+    return G;
 }
 
-void F3( void )
+int F3( void )
 {
+    int d = V[ 1 ][ 2 ] ^ V[ 1 ][ 3 ];
     
+    if (( G += d ) != V[ 1 ][ 3 ]) cout << "(illegal sequence)\t";
+    
+    return G;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "PFM algorithmic abstraction\n";
+    cout << "PFM algorithmic abstraction\n\n";
     
-    F1();
-    F2();
-    F3();
+    cout.flags( ios_base::left | ios_base::hex | ios_base::showbase );
+    
+    cout << "A vertex signatures assigned at compile time" << endl;
+    for ( int i = 0; i < 2; i++ ) {
+        for ( int j = 0; j < 4; j++ ) {
+            cout.width( 8 );
+            cout << hex << V[ i ][ j ] << "\t";
+        }
+        cout << endl;
+    }
+    
+    G = 0;                  // Next critical path evaluation, here optional
+    
+    cout << endl;
+    TRACE(F1()); cout << endl;
+    TRACE(F2()); cout << endl;
+    TRACE(F3()); cout << endl;
+    
+    G = 0;                  // GSR(G) = 0, next critical path evaluation
+    
+    cout << endl;
+    TRACE(F1()); cout << endl;
+    TRACE(F1()); cout << endl;
+    TRACE(F3()); cout << endl;
+    
+    G = 0;                  // GSR(G) = 0, next critical path evaluation
+    
+    cout << endl;
+    TRACE(F1()); cout << endl;
+    TRACE(F2()); cout << endl;
+    TRACE(F2()); cout << endl;
+    
+    G = 0;                  // GSR(G) = 0, next critical path evaluation
+    
+    cout << endl;
+    TRACE(F3()); cout << endl;
+    TRACE(F2()); cout << endl;
+    TRACE(F1()); cout << endl;
     
     return 0;
 }
